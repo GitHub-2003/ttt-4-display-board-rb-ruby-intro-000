69de25aa54c732c24cce30e91e061659d374a36f
MJU <marie.ju2012@outlook.com>
2020-01-14 06:09:52 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -146,7 +146,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" X | X | X ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" X | X | o ")
+      expect(rows[4]).to eq(" X | X | X ")
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -165,7 +165,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[1]).to eq("-----------")
       expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | O | X ")
+      expect(rows[4]).to eq(" O | O | O ")
 
       expect(true).to be(true)
     end
